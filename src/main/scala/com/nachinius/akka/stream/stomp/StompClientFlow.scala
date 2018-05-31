ed6262babc28ea059cc1d0126df578a8dc55fe61
Ignacio `nachinius` Peixoto <ignacio.peixoto@gmail.com>
2018-05-31 15:46:58 -0300

    refactor to use bidiflows for framing <-> bytestring and for connecting

--- src/main/scala/com/nachinius/akka/stream/stomp/StompClientFlow.scala
+++ src/main/scala/com/nachinius/akka/stream/stomp/StompClientFlow.scala
@@ -2,7 +2,7 @@ package com.nachinius.akka.stream.stomp
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.FlowShape
+import akka.stream.{BidiShape, FlowShape}
 import akka.stream.scaladsl.{Flow, Keep, Source, Tcp, _}
 import akka.stream.scaladsl.Tcp.OutgoingConnection
 import akka.util.ByteString
@@ -19,84 +19,84 @@ object StompClientFlow {
 
   def client(implicit settings: Settings): Flow[Frame, Frame, Future[OutgoingConnection]] = {
     //    StompClientConnection
-    framedFlow
+    stompConnectedFlow
   }
 
   def client(host: String, port: Int, tcp: Tcp): Flow[Frame, Frame, Future[OutgoingConnection]] = client(Settings(host, port, tcp))
 
-  def stompConnectedFlow(implicit settings: Settings): Flow[Frame, Frame, Future[OutgoingConnection]] = {
-    Flow.fromGraph(GraphDSL.create(framedFlow(settings)) { implicit b =>
-      tcpFlow =>
-        import GraphDSL.Implicits._
-
-        val connecting = b.add(Source.single(connectFrame(settings.host)))
-
-        val broadcast = b.add(Broadcast[Frame](2, false))
-        //      val mergePreferred = b.add(MergePreferred[Frame](1,false))
-        val concat = b.add(Concat[Frame](2))
-
-        connecting ~> concat ~> tcpFlow ~> broadcast
-
-        val processConnectionParameters = Sink.ignore
-        // handle "CONNECTED"
-        broadcast ~> Flow[Frame].dropWhile(!_.isConnected).take(1) ~> processConnectionParameters
-
-        // handle all the rest
-        val out = broadcast ~> Flow[Frame].collect({
-          case error@Frame(ERROR, _, _) => throw new StompProtocolError("Error frame received\n" + error.toString)
-          case msg@Frame(MESSAGE, _, _) => msg
-          case unknown@Frame(StompCommand.OTHER(str), _, _) => throw new StompProtocolError(s"Unknown stomp command received from server 'str'\n" + unknown.toString)
-          //        case _ => //ignored
-        })
-
-        // expose ports
-        FlowShape(concat.in(1), out.outlet)
-    })
-
-  }
+  /**
+    * Flow subscribed to a specific stomp server's topic
+    */
 
 
   /**
-    * Pure Flow connection to a TCP that as input and outputs has Frames
+    * Flow to a Stomp Server, where the 'connection' has been established
     *
     * @param settings
     * @return
     */
-  private[stomp] def framedFlow(implicit settings: Settings): Flow[Frame, Frame, Future[OutgoingConnection]] = {
-    val tcpFlow: Flow[ByteString, ByteString, Future[OutgoingConnection]] = settings.tcp.outgoingConnection(settings.host, settings.port)
-
-    toByteString.
-      via(debugFlow[ByteString]("\nSending-->\n" + _.utf8String + "\n<----")).
-      viaMat(tcpFlow)(Keep.right).
-      via(debugFlow("\nReceiving-->\n" + _.utf8String + "\n<----")).
-      via(fromByteString)
+  def stompConnectedFlow(implicit settings: Settings): Flow[Frame, Frame, Future[OutgoingConnection]] = {
+    connectToStompStep.atop(ByteStringFrameCodec).joinMat(tcpFlow)(Keep.right)
   }
 
-  private def debugFlow[A](msg: A => String): Flow[A, A, NotUsed] = Flow[A].map[A]({ elem => {
-    if (DEBUG) println("------>In debug flow:\n" + msg(elem) + "\n<<<<<<<<<<<<<")
-  };
-    elem
-  })
+  def connectToStompStep(implicit settings: Settings): BidiFlow[Frame, Frame, Frame, Frame, NotUsed] = {
+    BidiFlow.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      val connecting = b.add(Source.single(connectFrame(settings.host)))
+      val concat = b.add(Concat[Frame](2))
+
+      connecting ~> concat
+
+      val broadcast = b.add(Broadcast[Frame](2, false))
+
+      broadcast ~> Flow[Frame].dropWhile(f => !f.isConnected).take(1) ~> Sink.ignore
+
+      val out = broadcast ~> Flow[Frame].collect({
+        case msg@Frame(MESSAGE, _, _) => msg
+        case error@Frame(ERROR, _, _) => throw new StompProtocolError("Error frame received\n" + error.toString)
+        case unknown@Frame(StompCommand.OTHER(str), _, _) => throw new StompProtocolError(s"Unknown stomp command received from server 'str'\n" + unknown.toString)
+      })
+
+      BidiShape.of(concat.in(1), concat.out, broadcast.in, out.outlet)
+    })
+  }
+
+  /**
+    * Pure Flow connection to a Tcp connection that as input and outputs as Stomp Frames
+    *
+    * @param settings
+    * @return
+    */
+  private[stomp] def framedFlow(implicit settings: Settings) = {
+    ByteStringFrameCodec.joinMat(tcpFlow)(Keep.right)
+  }
+
+  private def tcpFlow(implicit settings: Settings): Flow[ByteString, ByteString, Future[OutgoingConnection]] = {
+    settings.tcp.outgoingConnection(settings.host, settings.port)
+  }
+
+  private def ByteStringFrameCodec = BidiFlow.fromFunctions(frameToByteString, byteStringToFrame)
 
   /**
     * Encode a ByteString to a Frame
     *
     * @return
     */
-  private def fromByteString: Flow[ByteString, Frame, NotUsed] = Flow.fromFunction((value: ByteString) => ParboiledImpl.decode(value.utf8String) match {
+  private def byteStringToFrame(value: ByteString): Frame = ParboiledImpl.decode(value.utf8String) match {
     case Right(frame) => frame
     case Left(msg) => throw new StompProtocolError("while decoding " + msg)
-  })
+  }
 
   /**
     * Decodes a Frame to a ByteString
     *
     * @return
     */
-  private def toByteString: Flow[Frame, ByteString, NotUsed] = Flow.fromFunction((value: Frame) => ParboiledImpl.encode(value) match {
+  private def frameToByteString(frame: Frame): ByteString = ParboiledImpl.encode(frame) match {
     case Right(str) => ByteString(str)
-    case Left(msg) => throw new StompProtocolError("while encoding " + msg)
-  })
+    case Left(msg) => throw new StompProtocolError("While encoding " + msg)
+  }
 
   /**
     * A Simple Frame message of type CONNECT
